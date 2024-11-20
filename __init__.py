e092b318e5ffd22934139c009f32d0eb20c685b5
aldjia899 <ouammour.aldjia@gmail.com>
2024-11-20 16:29:22 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,11 @@ from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)   
+app = Flask(__name__)    
+
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
 
 @app.route('/tawarano/')
 def meteo():
