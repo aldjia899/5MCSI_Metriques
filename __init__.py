033215551e2ea26233cc63fa9cb696b867522936
aldjia899 <ouammour.aldjia@gmail.com>
2024-11-20 16:12:21 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
-from flask import Flask, rendertemplatesting, render_template, jsonify
+from flask import Flask,
+render_template_string, 
+render_template, jsonify
 from flask import render_template
 from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)  
+app =
+Flask(__name__)  
+
 
 @app.route("/contact/")
 def MaPremiereAPI():
