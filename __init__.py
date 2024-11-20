c24552a171f8cdb01f453796795385fc65ac4e83
aldjia899 <ouammour.aldjia@gmail.com>
2024-11-20 16:16:00 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,11 @@
-from flask import Flask,
-render_template_string, 
-render_template, jsonify
+from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
-app =
-Flask(__name__)  
-
+app = Flask(__name__)                                                                                                                  
 
 @app.route("/contact/")
 def MaPremiereAPI():
@@ -17,7 +13,7 @@ def MaPremiereAPI():
 
 @app.route('/')
 def hello_world():
-  return render_template('hello.html') #Comm2
+    return render_template('hello.html')  #Comm2
   
 if __name__ == "__main__":
-app.run(debug=True)
+  app.run(debug=True)
